fb1ab3f168d2425bb554a9542a7e57092e31a49d
Sojib Das <sojibdas1015@gmail.com>
2026-02-05 20:54:54 +0600

    chas out charge calculate

---
+++ cashOut.js
@@ -0,0 +1,15 @@
+function cashOut(money) {
+    if (typeof money === 'number') {
+        if (money > 0) {
+            return money * 1.75 / 100;
+        }
+        else {
+            console.log("invalid");
+        }
+    }
+    else {
+        console.log("invalid");
+    }
+}
+const result = cashOut(4000);
+console.log(result);
